af2cf4c8b95b1c09480e47629dbe39dfa5d9af0a
Uma Sahni <monkeytown64@gmail.com>
2023-02-22 02:14:03 +0530

    backed Data

---
+++ index.js
@@ -0,0 +1,10 @@
+const jsonServer = require("json-server"); // importing json-server library
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
